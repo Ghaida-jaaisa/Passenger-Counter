4998da763b1077d267dd23ec558f7866c20b7b33
Ghaida Ja'aysah <ghaida.20004@gmail.com>
2025-05-16 07:47:06 +0300

    upload

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 let count = 0;
 let countEl = document.getElementById("count-el");
+let saveEl = document.getElementById("save-el");
 function increment() {
   count++;
-  countEl.innerText = count;
+  countEl.textContent = count;
 }
 
 function save() {
-
+saveEl.textContent += count + " - "
+countEl.textContent = 0
+count = 0
 }
